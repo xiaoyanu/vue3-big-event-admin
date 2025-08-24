be36e2ac08318405f26c3e7047ece02e51fbfe82
周星星 <1872786834@qq.com>
2025-08-24 10:06:22 +0800

    test

---
+++ src/views/layout/LayoutContainer.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineOptions({
+  name: 'LayoutContainer',
+})
+</script>
+
+<template>
+  <div>
+    布局架子
+    <router-view></router-view>
+  </div>
+</template>
+
+<style></style>
