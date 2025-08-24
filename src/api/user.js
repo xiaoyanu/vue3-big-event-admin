f99c1f5363c3b8d81151a7071e62ddc231d86739
周星星 <1872786834@qq.com>
2025-08-24 14:01:27 +0800

    写了点注释

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-// 注册接口啊
+// 注册接口啊的
 export const userRegisterService = ({ username, password, repassword }) => {
   return request.post('/api/reg', { username, password, repassword })
 }
