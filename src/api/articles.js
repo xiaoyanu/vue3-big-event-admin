c360e4440529a77a9c5f1f8f78dee0aca6a17033
周星星 <1872786834@qq.com>
2025-08-26 11:54:57 +0800

    day15

--- src/api/articles.js
+++ src/api/articles.js
@@ -14,3 +14,18 @@ export const artDelChannelService = (id) =>
   request.delete('/my/cate/del', {
     params: { id },
   })
+
+// 文章列表
+export const artGetListService = (params) => request.get('/my/article/list', { params })
+
+// 添加文章
+export const artPublishService = (data) => request.post('/my/article/add', data)
+
+// 获取文章详细
+export const artGetDetailService = (id) => request.get('my/article/info', { params: { id } })
+
+// 编辑文章
+export const artEditService = (data) => request.put('my/article/info', data)
+
+// 删除文章
+export const artDelService = (id) => request.delete('my/article/info', { params: { id } })
--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -1,69 +1,119 @@
 <script setup>
 import { Delete, Edit } from '@element-plus/icons-vue'
 import { ref } from 'vue'
-// 假数据
-const articleList = ref([
-  {
-    id: 5961,
-    title: '新的文章啊',
-    pub_date: '2022-07-10 14:53:52.604',
-    state: '已发布',
-    cate_name: '体育',
-  },
-  {
-    id: 5962,
-    title: '新的文章啊',
-    pub_date: '2022-07-10 14:54:30.904',
-    state: null,
-    cate_name: '体育',
-  },
-])
+import ChannelSelect from './components/ChannelSelect.vue'
+import { artGetListService } from '@/api/articles'
+import { formatTime } from '@/utils/format'
+import ArticleEdit from './components/ArticleEdit.vue'
+const articleList = ref([]) //文章列表
+const total = ref(0) //总条数
+// 自定义请求参数对象
+const params = ref({
+  pagenum: 1,
+  pagesize: 5,
+  cate_id: '',
+  state: '',
+})
+
+const articleEditRef = ref()
+
+// loading
+const loading = ref(false)
 
 // 编辑逻辑
 const onEditArticle = (row) => {
+  articleEditRef.value.open(row)
   console.log(row)
+}
+
+// 添加逻辑
+const onAddArticle = () => {
+  articleEditRef.value.open({})
 }
 
 // 删除逻辑
 const onDeleteArticle = (row) => {
   console.log(row)
 }
+const getArticleList = async () => {
+  loading.value = true
+  const res = await artGetListService(params.value)
+  articleList.value = res.data.data
+  total.value = res.data.total
+  loading.value = false
+}
+getArticleList()
+
+// 分页逻辑
+const onSizeChange = (size) => {
+  params.value.pagenum = 1
+  params.value.pagesize = size
+  getArticleList()
+}
+const onCurrentChange = (page) => {
+  params.value.pagenum = page
+  getArticleList()
+}
+
+// 搜索处理
+const onSearch = () => {
+  params.value.pagenum = 1
+  getArticleList()
+}
+
+const onReset = () => {
+  params.value.pagenum = 1
+  params.value.cate_id = ''
+  params.value.state = ''
+  getArticleList()
+}
+
+// 添加修改成功
+const onSuccess = (type) => {
+  if (type === 'add') {
+    // 如果是添加，需要跳转渲染最后一页，编辑直接渲染当前页
+    const lastPage = Math.ceil((total.value + 1) / params.value.pagesize)
+    params.value.pagenum = lastPage
+  }
+  getArticleList()
+}
 </script>
 <template>
   <page-container title="文章管理">
     <template #extra>
-      <el-button>添加文章</el-button>
+      <el-button type="primary" @click="onAddArticle">添加文章</el-button>
     </template>
 
     <!-- 表单区域 -->
     <el-form inline>
       <el-form-item label="文章分类：">
-        <el-select style="width: 220px">
-          <el-option label="新闻" value="111"></el-option>
-          <el-option label="体育" value="222"></el-option>
-        </el-select>
+        <channel-select v-model="params.cate_id"></channel-select>
       </el-form-item>
       <el-form-item label="发布状态：">
-        <el-select style="width: 220px">
+        <el-select style="width: 220px" v-model="params.state">
           <el-option label="已发布" value="已发布"></el-option>
           <el-option label="草稿" value="草稿"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">搜索</el-button>
-        <el-button>重置</el-button>
+        <el-button @click="onSearch" type="primary">搜索</el-button>
+        <el-button @click="onReset">重置</el-button>
       </el-form-item>
     </el-form>
 
     <!-- 表格区域 -->
-    <el-table :data="articleList" style="width: 100%">
+    <el-table :data="articleList" style="width: 100%" v-loading="loading">
       <el-table-column label="文章标题" width="400">
         <template #default="{ row }">
           <el-link type="primary" :underline="false">{{ row.title }}</el-link>
         </template>
       </el-table-column>
       <el-table-column label="分类" prop="cate_name"></el-table-column>
-      <el-table-column label="发表时间" prop="pub_date"> </el-table-column>
+      <el-table-column label="发表时间" prop="pub_date">
+        <template #default="{ row }">
+          {{ formatTime(row.pub_date) }}
+        </template>
+      </el-table-column>
       <el-table-column label="状态" prop="state"></el-table-column>
       <el-table-column label="操作" width="100">
         <template #default="{ row }">
@@ -87,7 +137,23 @@ const onDeleteArticle = (row) => {
         <el-empty description="没有数据" />
       </template>
     </el-table>
+
+    <!-- 分页区域 -->
+    <el-pagination
+      v-model:current-page="params.pagenum"
+      v-model:page-size="params.pagesize"
+      :page-sizes="[2, 3, 4, 5, 10]"
+      layout="jumper, total, sizes, prev, pager, next"
+      background
+      :total="total"
+      @size-change="onSizeChange"
+      @current-change="onCurrentChange"
+      style="margin-top: 20px; justify-content: flex-end"
+    />
   </page-container>
+
+  <!-- 抽屉 -->
+  <ArticleEdit ref="articleEditRef" @success="onSuccess"></ArticleEdit>
 </template>
 
 <style scoped></style>
